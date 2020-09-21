d159e2288d9f971dcbaff564b417c02be6998f95
gness9 <42481860+gness9@users.noreply.github.com>
2020-09-21 01:30:57 -0500

    make it better

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+});
+client.on("message", (msg) => {
+    if (msg.content === "ping") {
+        msg.reply("Pong!")
+    }
+});
+
+client.on("guildMemberAdd", (member) => {
+    member.send(
+        `Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
+    )
+});
+
+
+client.login(process.env.BOT_TOKEN);
